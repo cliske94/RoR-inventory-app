e1e8b92461efb852602333b8a2715d8506c66654
Cody Liske <cliske01@email.cpcc.edu>
2018-09-08 19:08:33 -0400

    trying to get inventory pages to work, postgresql server does not seem to be running

--- db/migrate/20180908223652_create_inventories.rb
+++ db/migrate/20180908223652_create_inventories.rb
@@ -1,9 +1,10 @@
 class CreateInventories < ActiveRecord::Migration
   def change
     create_table :inventories do |t|
+      t.integer :id
       t.string :name
       t.text :description
-      t.int :quantity
+      t.integer :quantity
 
       t.timestamps null: false
     end
