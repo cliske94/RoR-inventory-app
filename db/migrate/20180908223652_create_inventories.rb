61cecd0991f79f20db03c6566b0baf271326f393
Cody Liske <cliske01@email.cpcc.edu>
2018-09-12 18:47:44 -0400

    postgres

--- db/migrate/20180908223652_create_inventories.rb
+++ db/migrate/20180908223652_create_inventories.rb
@@ -1,7 +1,6 @@
 class CreateInventories < ActiveRecord::Migration
   def change
     create_table :inventories do |t|
-      t.integer :id
       t.string :name
       t.text :description
       t.integer :quantity
