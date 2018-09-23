7a998c372acd01d756469a17d112f2146c420f71
Cody Liske <cliske01@email.cpcc.edu>
2018-09-22 23:03:20 -0400

    added and enabled javascript in the asset pipeline, added a fixed background image, and a right hand side company news bulletin

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+// Set vars for column content
+//= require_self
+//=
+
---
+++ app/assets/javascripts/test.js
@@ -0,0 +1,31 @@
+//= require_self
+//= require_tree
+//= require_directory
+
+$(document).ready(
+function function1 () {
+ // alert("I'm working!");
+  var colOne = $('.col-1').nextUntil('.col-2').addBack();
+  var colTwo = $('.col-2').nextAll().addBack();
+
+// Append new divs that will take the column content
+  $('.wrap').append('<div class="column-first group" /><div class="column-second ground" />');
+
+// Append column content to new divs
+  $(colOne).appendTo('.column-first');
+  $(colTwo).appendTo('.column-second');
+
+// Get the .wrap height after all this is done
+  var wrapHeight = $('.wrap').outerHeight();
+  var colHeight;
+
+// Calc height of each col
+  for (var i = 0; i < $('.col-1').length; i++) {
+    var eachCol = $('.col-1').outerHeight();
+    colHeight = eachCol*i;
+  }  
+
+// Add the heights
+$('.wrap').height(colHeight+wrapHeight);
+}
+  );
