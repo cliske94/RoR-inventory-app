25996fda02e2b6707f57df22be370c5a048f99e2
Cody Liske <cliske01@email.cpcc.edu>
2018-12-02 23:47:33 -0500

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
-  
+  validates :username, uniqueness: { case_sensitive:false }, presence: true, 
+            length: { minimum: 3, maximum: 25 }
+  VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, uniqueness: { case_sensitive: false}, presence: true,
+  format: { with: VALID_EMAIL_REGEX }
 end
